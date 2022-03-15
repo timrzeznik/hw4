3137bfb47f74cfe69374657f4d02e61e48981194
timrzeznik <tim.rzeznik@kellogg.northwestern.edu>
2022-03-15 01:48:29 +0000

    initial commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params["post"])
-    @post.save
-    redirect_to "/places/#{@post.place.id}"
+    if @current_user
+      @post = Post.new(params["post"])
+      @post.user_id = @current_user.id
+      @post.save
+    else 
+      flash[:notice] = "Please login first."
+    end
+      redirect_to "/places/#{@post.place.id}"
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ email: params["email"] })
+    if @user
+      if BCrypt::Password.new(@user.password) == params["password"]
+        session[:user_id] = @user.id
+        flash[:notice] = "Welcome, #{@user.username}."
+        redirect_to "/places"
+      else
+        flash[:notice] = "Nope."
+        redirect_to "/sessions/new"
+      end
+    else
+      flash[:notice] = "Email has not been registered"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Goodbye."
+    redirect_to "/sessions/new"
   end
 end
-  
